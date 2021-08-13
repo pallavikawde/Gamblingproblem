f89780552b7160e1cbcc4de6a5011465db75d229
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-08-13 12:50:22 +0530

    Added:uc1_values for stake and beet

--- src/com/bridlabz/Gamblingprocess.java
+++ src/com/bridlabz/Gamblingprocess.java
@@ -1,6 +1,11 @@
 package com.bridlabz;
 
 public class Gamblingprocess {
+
+    //uc1
+    public static final int STAKE=100;
+    public static final int BET=1;
+    
     public static void main(String[] args) {
         System.out.println("welcome to gambler");
 
