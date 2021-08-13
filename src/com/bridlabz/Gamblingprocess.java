4aba315f6aada8403737edfd851b0438643a7d41
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-08-13 14:37:47 +0530

    Added:gambler wining or lossing condition

--- src/com/bridlabz/Gamblingprocess.java
+++ src/com/bridlabz/Gamblingprocess.java
@@ -1,14 +1,30 @@
 package com.bridlabz;
 
+import java.util.Random;
+
 public class Gamblingprocess {
 
     //uc1
     public static final int STAKE=100;
     public static final int BET=1;
-    
+    public static final float STAKE_VALUE = 0.5F;
+    public int wining_value,stake,loosingvalue;
+//uc2
+    public void winOrLoss(){
+        Random random= new Random();
+        int value = random.nextInt(2);
+        if(value == 1){
+            System.out.println("****gambler won******");
+            stake++;
+        }else{
+            System.out.println("*****gambler loss****");
+            stake--;
+        }
+    }
     public static void main(String[] args) {
         System.out.println("welcome to gambler");
-
+        Gamblingprocess gambler=new Gamblingprocess();
+        gambler.winOrLoss();
         }
     }
 
