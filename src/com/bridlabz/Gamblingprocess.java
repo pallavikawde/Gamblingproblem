07da14bfb5104a04655d5f58536265d2533f1837
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-08-13 14:51:50 +0530

    Added:resignstacke

--- src/com/bridlabz/Gamblingprocess.java
+++ src/com/bridlabz/Gamblingprocess.java
@@ -8,7 +8,7 @@ public class Gamblingprocess {
     public static final int STAKE=100;
     public static final int BET=1;
     public static final float STAKE_VALUE = 0.5F;
-    public int wining_value,stake,loosingvalue;
+    public int wining_value,stake,loosing_value;
 //uc2
     public void winOrLoss(){
         Random random= new Random();
@@ -21,10 +21,27 @@ public class Gamblingprocess {
             stake--;
         }
     }
+    public int resignStake(){
+        loosing_value = (int)Math.round(STAKE*STAKE_VALUE);
+        wining_value=(int)Math.round(STAKE+(STAKE*STAKE_VALUE));
+        boolean stop=true;
+        stake=STAKE;
+        while (stop == true){
+            winOrLoss();
+            if(stake == loosing_value){
+                stop=false;
+            }
+            if(stake == wining_value){
+                stop=false;
+            }
+        }
+        return stake;
+    }
     public static void main(String[] args) {
         System.out.println("welcome to gambler");
         Gamblingprocess gambler=new Gamblingprocess();
         gambler.winOrLoss();
+        gambler.resignStake();
         }
     }
 
